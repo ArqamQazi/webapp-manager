07c7d7f3b8d659273ab2181d0c6f34b55fd4dce9
ArqamQazi <arqamqazi549@gmail.com>
2026-08-18 18:29:59 +0530

    anylinux appimage support

--- .github/scripts/get-dependencies.sh
+++ .github/scripts/get-dependencies.sh
@@ -9,15 +9,17 @@ sudo pacman -Syu --noconfirm base-devel wget gettext python python-gobject pytho
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-if ! command -v get-debloated-pkgs >/dev/null 2>&1; then
-    wget "https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh" -O ./get-debloated-pkgs
-    chmod +x ./get-debloated-pkgs
-    ./get-debloated-pkgs --add-common --add-mesa --prefer-nano
-else
-    get-debloated-pkgs --add-common --add-mesa --prefer-nano
-fi
+get-debloated-pkgs --add-common --prefer-nano
 
-echo "Installing webapp-manager..."
-echo "---------------------------------------------------------------"
-make all
-sudo cp -r usr/* /usr/
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
--- .github/scripts/make-appimage.sh
+++ .github/scripts/make-appimage.sh
@@ -7,30 +7,28 @@ if [ "${GITHUB_EVENT_NAME:-}" = "release" ]; then
 else
   export VERSION=$(head -n 1 debian/changelog | awk -F '[()]' '{print $2}')
 fi
-
-# Clean previous build artifacts
-rm -rf ./AppDir ./dist
-
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/webapp-manager.svg
 export DESKTOP=/usr/share/applications/webapp-manager.desktop
 export DEPLOY_PYTHON=1
-export ALWAYS_SOFTWARE=1
-export APPNAME="Webapp Manager"
+export PATH_MAPPING='
+	/usr/share/webapp-manager:${SHARUN_DIR}/share/webapp-manager
+	/usr/share/locale:${SHARUN_DIR}/share/locale
+'
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-SHARUN_URL="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-wget "$SHARUN_URL" -O ./quick-sharun
+wget "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-
-./quick-sharun /usr/bin/webapp-manager /usr/lib/webapp-manager /usr/share/webapp-manager/ /usr/lib/libgtk-3.so.0
-
-# Fix the wrapper script to use relative paths and remove background execution
-sed -i 's|/usr/lib/webapp-manager|${SHARUN_DIR}/lib/webapp-manager|g' ./AppDir/bin/webapp-manager
-sed -i 's| &||g' ./AppDir/bin/webapp-manager
-
-# Create usr symlinks so anylinux.so and unshare path interception works flawlessly
-# (Removed as per anylinux philosophy: we patch the app instead)
-sed -i 's|os.environ.get("APPDIR", "") + sys.prefix|os.environ.get("APPDIR", "")|g' ./AppDir/lib/webapp-manager/webapp-manager.py
+# Deploy dependencies
+./quick-sharun \
+  /usr/bin/webapp-manager \
+  /usr/lib/webapp-manager \
+  /usr/share/webapp-manager \
+  /usr/lib/libgtk-3.so* \
+  /usr/lib/libxapp.so*
 
 ./quick-sharun --make-appimage
+
+./quick-sharun --simple-test ./dist/*.AppImage
